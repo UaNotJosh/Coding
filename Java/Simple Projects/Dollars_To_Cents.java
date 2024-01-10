3a3799a0ef86a4fce455a61c4120e127e94937ee
Joshua <JRichardson8324@gmail.com>
2024-01-10 17:00:53 -0500

    Add frame for next project

---
+++ Java/Simple Projects/Dollars_To_Cents.java
@@ -0,0 +1,11 @@
+//Converts input dollar amount into cents and gives coins to value
+//Import necessary utilities
+import java.util.Scanner;
+
+public class Dollars_To_Cents {
+    public static void main(String[] args){
+        //Initialize variables
+
+        //Ask user for input
+    }
+}
